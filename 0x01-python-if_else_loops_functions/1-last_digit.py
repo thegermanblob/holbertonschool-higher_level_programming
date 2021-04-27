c0fb55719cb29cbac6a4d68d7188e3d5396f7bcd
Thegermanblob <thegermanblob@gmail.com>
2021-04-27 19:35:48 +0000

    Add last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,13 +5,12 @@ neg = True
 print("Last digit of {:d} is".format(number), end=" ")
 if number < 10 and number > -10:
     print("{:d}".format(number), end=" ")
-if number < 0:
+elif number < 0:
     print("-", end="")
     number = number * -1
     neg = False
 
 number = number % 10
-
 if number > 5 and neg:
     print("{:.0f} and is greater than 5".format(number))
 elif number is 0 and neg:

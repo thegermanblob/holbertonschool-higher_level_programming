d4b066f15b47bc1ab1350ee431fcbc388f795cfc
Thegermanblob <thegermanblob@gmail.com>
2021-04-27 20:48:38 +0000

    Fix check

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if i != 9 or j != 9:
-            print("{}{}, ".format(i, j), end="")
+for i in range(0, 100):
+        if i != 99:
+            print("{}, ".format(i), end="")
         else:
-            print("{}{}".format(i, j))
+            print("{}".format(i))

11b973872883971ee92f65a18950732420f5b01c
Thegermanblob <thegermanblob@gmail.com>
2021-04-27 20:56:15 +0000

    fix end line:

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(0, 10):
     for j in range(0, 10):
         if i <= j:
             if i != 9 or j != 9:
-                print("{}{}, ".format(i, j))
+                print("{}{}, ".format(i, j), end="")
             else:
                 print("{}{}".format(i, j))

9d0539112b151b4be970555a072f53c8abb90465
Thegermanblob <thegermanblob@gmail.com>
2021-04-27 23:35:52 +0000

    Add fizbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,5 +5,7 @@ def fizzbuzz():
             print("Fizz", end=" ")
         elif i % 5 == 0 and i % 3 != 0:
             print("Buzz", end=" ")
+        elif i % 5 == 0 and i % 3 == 0:
+            print("FizzBuzz")
         else:
             print("{}".format(i), end=" ")

85ffbc495694bde9e667d53fcaf1ed4c62e68253
Thegermanblob <thegermanblob@gmail.com>
2021-04-28 01:23:33 +0000

    add file

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    print("{:c}".format(i), end="")
+    if i % 2 == 0:
+        print("{:c}".format(i), end="")
+    else:
+        print("{:c}".format(i - 32), end="")

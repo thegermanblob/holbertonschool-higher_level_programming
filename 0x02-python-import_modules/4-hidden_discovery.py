6704f5cbc8e8e74e2daf76b60b40e9219dfcfced
Thegermanblob <thegermanblob@gmail.com>
2021-04-29 16:42:16 +0000

    make pepe happy

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-    dirlist  = dir(hidden_4)
+    dirlist = dir(hidden_4)
     for i in range(0, len(dirlist)):
         if dirlist[i][1] != "_":
             print("{}".format(dirlist[i]))

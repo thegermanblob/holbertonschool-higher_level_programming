565bddb4b648dcf795737b1ec033a0d1c0ca966e
Thegermanblob <thegermanblob@gmail.com>
2021-04-29 16:31:08 +0000

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     i = 1
     if len(argv) == 1:
         print("0 arguments.")
+    elif len(argv) == 2:
+        print("{:d} argument:".format(len(argv) - 1))
     else:
         print("{:d} arguments:".format(len(argv) - 1))
     while i + 1 < len(argv) + 1:

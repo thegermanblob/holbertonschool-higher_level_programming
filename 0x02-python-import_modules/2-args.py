4cc0ca04bdda99bbbc85ee852dd16de8e8c667b7
Thegermanblob <thegermanblob@gmail.com>
2021-04-29 16:27:02 +0000

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 from sys import argv
-i = 1
-if len(argv) == 1:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(len(argv) - 1))
-while i + 1 < len(argv) + 1:
-    print("{}: {}".format(i, argv[i]))
-    i = i + 1
+if __name__ == "__main__":
+    i = 1
+    if len(argv) == 1:
+        print("0 arguments.")
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+    while i + 1 < len(argv) + 1:
+        print("{:d}: {}".format(i, argv[i]))
+        i = i + 1

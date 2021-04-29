2bf2cee58dcb0d0fbced7aa55f060ec1ca64be49
Thegermanblob <thegermanblob@gmail.com>
2021-04-29 16:35:20 +0000

    infinte

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 from sys import argv
-r = 0
-for i in range(1, len(argv)):
-     r = r + int(argv[i])
-
-
-print("{}".format(r))
+if __name__ == "__main__":
+    r = 0
+    for i in range(1, len(argv)):
+         r = r + int(argv[i])
+    print("{:d}".format(r))
 

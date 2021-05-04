fb739aaca6382cf605c8373a53dc064afd1b1df3
Thegermanblob <thegermanblob@gmail.com>
2021-05-04 22:27:29 +0000

    Fix check 7,9,11

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     en = len(my_list) - 1
     if(idx > en or idx < 0):
-        return(None)
+        return(my_list)
     elif(my_list is None):
         return(None)
     elif(element is None):

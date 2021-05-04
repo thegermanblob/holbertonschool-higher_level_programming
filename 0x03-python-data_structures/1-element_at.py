be7e0ce24f78f1e7285ddec71f19f7f25f1f06c5
Thegermanblob <thegermanblob@gmail.com>
2021-05-04 21:54:55 +0000

    Add element at

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,5 +3,7 @@ def element_at(my_list, idx):
     en = len(my_list)
     if(idx > en or idx < 0):
         return(None)
+    elif (!my_list):
+        return(None)
     else:
         return(my_list[idx])

fd236418fbf6e4858ba975fb58c764ce717d7a78
Thegermanblob <thegermanblob@gmail.com>
2021-05-05 14:31:09 +0000

    Make pepe happy

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
         for val in range(len(matrix[row])):
             print("{:d}".format(matrix[row][val]), end="")
-            if val <= (len(matrix[row]) - 1):
+            if val < (len(matrix[row]) - 1):
                 print("", end=" ")
         print("\n", end="")

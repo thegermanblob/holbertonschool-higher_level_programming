5b3a89bb7542ecfcd9b962e9c61518542b833e11
Thegermanblob <thegermanblob@gmail.com>
2021-05-05 00:53:36 +0000

    fix int error was iterating list items not ints

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,9 @@
 def print_matrix_integer(matrix=[[]]):
     if (matrix is None):
         return(None)
-    for row in matrix:
-        for val in row:
-            print ('{:d}'.format(val), end="")
-            if (val % 3 != 0):
+    for row in range(len(matrix)):
+        for val in range(len(matrix[row])):
+            print ('{:d}'.format(matrix[row][val]), end="")
+            if val != (len(matrix[row]) - 1):
                 print("", end=" ")
         print("")

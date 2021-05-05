efbe2096c716b87583e97650858c53a783eff1b5
Thegermanblob <thegermanblob@gmail.com>
2021-05-05 00:39:52 +0000

    Fix None edge case

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if (matrix is None):
+        return(None)
     for row in matrix:
         for val in row:
             print ('{:d}'.format(val), end=" ")

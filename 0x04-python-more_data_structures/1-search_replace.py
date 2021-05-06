c7e30bc479095485a9f04b3b49958e5ae09d740d
Thegermanblob <thegermanblob@gmail.com>
2021-05-06 20:25:18 +0000

    Add search replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    nlist = my_list [:]
+    i = nlist.index(search)
+    nlist.remove(i)
+    nlist.insert(i, replace)
+    return nlist

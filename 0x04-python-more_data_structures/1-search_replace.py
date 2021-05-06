e1c51489a9638e755b393f560f61312ad1bb8ea8
Thegermanblob <thegermanblob@gmail.com>
2021-05-06 20:47:03 +0000

    Add search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    nlist = my_list[:]
-    i = nlist.index(search)
-    nlist.remove(i)
-    nlist.insert(i, replace)
+    nlist = my_list [:]
+    for item in nlist:
+        if item == search:
+            i = nlist.index(search)
+            nlist.remove(search)
+            nlist.insert(i, replace)
     return nlist

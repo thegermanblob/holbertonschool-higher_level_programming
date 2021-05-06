9a31027f4697003588d1e9ddd07bca46c2e25ac9
Thegermanblob <thegermanblob@gmail.com>
2021-05-06 20:28:39 +0000

    Make pepe happy

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    nlist = my_list [:]
+    nlist = my_list[:]
     i = nlist.index(search)
     nlist.remove(i)
     nlist.insert(i, replace)

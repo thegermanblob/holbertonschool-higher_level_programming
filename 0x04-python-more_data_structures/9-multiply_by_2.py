1ff207ada9cdbf39182096410dccbdf2feecf04b
Thegermanblob <thegermanblob@gmail.com>
2021-05-07 01:45:03 +0000

    Add multiple

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    b_dictionary = {}
+    for key in a_dictionary.keys():
+        b_dictionary[key] = a_dictionary[key] * 2
+    return(b_dictionary)

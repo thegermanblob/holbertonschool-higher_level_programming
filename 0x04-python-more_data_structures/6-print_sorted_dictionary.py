03b59bc080503c1c54ec665ea16f580f497e4e1c
Thegermanblob <thegermanblob@gmail.com>
2021-05-07 00:27:15 +0000

    Changed method to be able to print any data type

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    b_dict = a_dictionary.copy()
-    for key, value in sorted(b_dict.items()):
-        print(key, ':', value)
+    for key in sorted(a_dictionary):
+            print("{}:{}".format(key, a_dictionary[key]))

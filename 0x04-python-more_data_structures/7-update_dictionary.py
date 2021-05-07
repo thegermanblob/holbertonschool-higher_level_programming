9fdad3a6bf8012bd17699b0d67dd4bc339859462
Thegermanblob <thegermanblob@gmail.com>
2021-05-07 00:44:50 +0000

    Fix syntax error

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key != None and value != None:
+    if key is not None and value is not None:
         a_dictionary[key] = value
         return(a_dictionary)

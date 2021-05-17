e326a92ea6df5cfaf998cd2cf7add994c1f92b07
Thegermanblob <thegermanblob@gmail.com>
2021-05-17 22:46:25 +0000

    Add fianlly to fufill req

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,6 +14,6 @@ def list_division(my_list_1, my_list_2, list_length):
             result_list.append(0)
             print("out of range")
         finally:
-            print("")
+            print("", end="")
 
     return(result_list)

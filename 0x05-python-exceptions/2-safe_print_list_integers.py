dc53e4ffbcde0e05d8c4ab559eb066585daa8566
Thegermanblob <thegermanblob@gmail.com>
2021-05-17 20:32:12 +0000

    Change exeptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    printed = 0
     for idx, item in enumerate(my_list):
         try:
             print("{:d}".format(my_list[idx]), end='')
-        except:
-            print("", end='')
-        idx += 1
+            printed += 1
+        except (ValueError, TypeError):
+            continue
+
+
     print('')
 
-    return idx
+    return printed

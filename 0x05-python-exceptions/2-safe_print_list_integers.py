693be2db8b5abf0990b2373ec348c3244cbafc24
Thegermanblob <thegermanblob@gmail.com>
2021-05-17 20:45:56 +0000

    Make pep8 compliant

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     printed = 0
-    for idx in range(0,x):
+    for idx in range(0, x):
         try:
             print("{:d}".format(my_list[idx]), end='')
             printed += 1
         except (ValueError, TypeError):
             continue
-
 
     print('')
 

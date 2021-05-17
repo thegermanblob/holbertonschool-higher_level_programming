f8efaa3acde946eac41bc8d186a5cdfb571c5ff7
Thegermanblob <thegermanblob@gmail.com>
2021-05-17 18:10:14 +0000

    Make pep8 compliant

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     idx = 0
     while(idx < x):
         try:
-            print("{:d}".format(my_list[idx]),end='')
+            print("{:d}".format(my_list[idx]), end='')
         except:
             break
         idx += 1

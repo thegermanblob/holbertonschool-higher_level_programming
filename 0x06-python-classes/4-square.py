a91ce221319c7c165d3fa2f66d60aab9e6b0783b
Thegermanblob <thegermanblob@gmail.com>
2021-05-19 21:01:20 +0000

    Make pep8 compliant

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -21,16 +21,16 @@ class Square:
         result = self.__size ** 2
         return result
 
-
     @property
     def size(self):
+        """ gets the size of square"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ sets the new value for size """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-

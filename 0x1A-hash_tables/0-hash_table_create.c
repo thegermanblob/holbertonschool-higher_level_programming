47566ae6b66bb87d6a7a8d3d70f9750fef42e4f2
Thegermanblob <thegermanblob@gmail.com>
2021-05-28 23:14:08 +0000

    Add task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ *
+ *
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int i;
+
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return(NULL);
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!table->array)
+		return (NULL);
+
+	for ( i = 0; i < size; i++)
+		table->array[i] = NULL;
+
+
+
+	return (table);
+}

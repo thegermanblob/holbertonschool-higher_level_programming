d966c8145fc3486f1c24d6fdcd1f9a035df632f6
Thegermanblob <thegermanblob@gmail.com>
2021-06-02 16:55:24 +0000

    Add task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" module that contains the Mylist class """
+
+class MyList(list):
+    """ New list based class """
+
+    def print_sorted(self):
+        print(sorted(self))

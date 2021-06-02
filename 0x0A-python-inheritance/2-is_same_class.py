f60442f98e8398fb5dab07f576408d298f6162e6
Thegermanblob <thegermanblob@gmail.com>
2021-06-02 17:22:53 +0000

    Add task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Module containing is same function """
+
+
+def is_same_class(obj, a_class):
+    """ function that returns if obj is instance of a_class """
+    return type(obj) == a_class

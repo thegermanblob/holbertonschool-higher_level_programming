8d7d395e02a4ffe98ff9a41979aaf72adb9634cb
Thegermanblob <thegermanblob@gmail.com>
2021-06-03 23:04:21 +0000

    Add documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ Module contaning the read_file function """
 
+
 def read_file(filename=""):
+    """ Function that opens a file """
 
     with open(filename, 'r+') as file:
         pass

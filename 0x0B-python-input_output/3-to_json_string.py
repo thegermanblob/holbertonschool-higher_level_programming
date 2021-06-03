61aba1c11c1fb2c5d187c86059d779d8308c19b5
Thegermanblob <thegermanblob@gmail.com>
2021-06-03 23:19:12 +0000

    Make pep8 compliant

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import json
 """ MOdule that contains the to_json_string function """
 
 
 def to_json_string(my_obj):
     """ Function that makes json strings from given obj """
 
+    import json
     return json.dumps(my_obj)

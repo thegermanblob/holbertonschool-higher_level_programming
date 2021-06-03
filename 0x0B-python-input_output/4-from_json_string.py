d23feba80a153f74b5622d736ccb652f6d88b683
Thegermanblob <thegermanblob@gmail.com>
2021-06-03 23:21:06 +0000

    Make task 4 pep8 compliant

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import json
 """ Module that contains the from_json_string """
 
 
 def from_json_string(my_str):
+    """ Function that reads from a json string """
+    import json
     return json.loads(my_str)

bf602392c818463e252657eac177fdffea7c6ae9
Thegermanblob <thegermanblob@gmail.com>
2021-07-13 20:35:06 +0000

    Add task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- creates second_table
+-- and adds 4 new records
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO `second_table` (`id`,`name`,`score`) VALUES (1, "John",  10);
+INSERT INTO `second_table` (`id`,`name`,`score`) VALUES (2,"Alex", 3);
+INSERT INTO `second_table` (`id`,`name`,`score`) VALUES (3,"Bob", 14);
+INSERT INTO `second_table` (`id`,`name`,`score`) VALUES (4,"George", 8);

c68d4d83371b04a97c4ea0fdb25eb425db45c783
Thegermanblob <thegermanblob@gmail.com>
2021-07-14 21:34:52 +0000

    Fix indentify

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Creates new user: user_0d_1
 -- sets password to: user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILIGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILIGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILIGES;

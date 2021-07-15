1c553f3b572cf1716329ba66db5f625a226a4887
Thegermanblob <thegermanblob@gmail.com>
2021-07-15 00:38:03 +0000

    Add ;

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 --creates a table called id_not_null
-CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));

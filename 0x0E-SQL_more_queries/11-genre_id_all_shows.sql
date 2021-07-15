072e56946024b56c9b6907e774ba1ac9033b56fc
Thegermanblob <thegermanblob@gmail.com>
2021-07-15 01:50:08 +0000

    Add task 11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- lists all shows that have atleast one genre
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+left JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;

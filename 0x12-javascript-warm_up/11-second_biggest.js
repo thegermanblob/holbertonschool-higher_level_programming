02ad68a15350c54e2faa2a60ac9ab48abf3983e1
Thegermanblob <thegermanblob@gmail.com>
2021-08-02 22:49:41 +0000

    Fix no arg edge case

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,10 +3,11 @@ if (isNaN(process.argv[2])) {
   console.log(0);
 } else if (process.argv.length === 3) {
   console.log(0);
-}
+}else {
 const array = process.argv;
 array.shift();
 array.shift();
 const arrsorted = [...new Set(array)];
 const arrsort = arrsorted.sort((a, b) => a - b);
 console.log(arrsort[arrsort.length - 2]);
+}

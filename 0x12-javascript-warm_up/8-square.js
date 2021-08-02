e258d88ccfe91adb48a5887183a47951f5596ed3
Thegermanblob <thegermanblob@gmail.com>
2021-08-02 20:53:31 +0000

    Add task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let idx1 = 0; idx1 < process.argv[2]; idx1++) {
+    let str = '';
+    for (let idx2 = 0; idx2 < process.argv[2]; idx2++) {
+      str += 'x';
+    }
+    console.log(str);
+  }
+}

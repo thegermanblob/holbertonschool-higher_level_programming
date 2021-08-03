1473fbdc5a29340a8b2421e045fd7de532d2e711
Thegermanblob <thegermanblob@gmail.com>
2021-08-03 15:53:47 +0000

    Add task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!usr/bin/nodeZ
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (!isNaN(h) && !isNaN(w) && (h > 0 && w > 0)) {
+      this.height = h;
+      this.width = w;
+    }
+  }
+
+  print () {
+    for (let y = 0; y < this.height; ++y) {
+      for (let x = 0; x < this.width; ++x) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};

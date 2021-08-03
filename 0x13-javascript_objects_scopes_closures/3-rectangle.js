090d3d2abf09fb045303bfe52a7ae4c9f7afe137
Thegermanblob <thegermanblob@gmail.com>
2021-08-03 18:12:19 +0000

    Fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,8 +2,8 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (!isNaN(h) && !isNaN(w) && (h > 0 && w > 0)) {
-      this.height = h;
       this.width = w;
+      this.height = h;
     }
   }
 

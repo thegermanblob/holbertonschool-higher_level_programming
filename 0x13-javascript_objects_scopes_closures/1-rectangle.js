8a6e0235e3b745e038733750e3560cc4faefef93
Thegermanblob <thegermanblob@gmail.com>
2021-08-03 17:55:18 +0000

    Fix variables

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    this.height = height;
-    this.width = width;
+    this.height = h;
+    this.width = w;
   }
-}
+};

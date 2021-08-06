5e535af441bcd703a0e553e8ab82bac8be491e74
Thegermanblob <thegermanblob@gmail.com>
2021-08-06 19:17:57 +0000

    Add task 6 (model state)

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Module contains the State class """
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ Class represents the table states from Db hbtn_0e_4_usa"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    def __repr__(self):
+        """ Method for string representation """
+        return "State: {}, {}".format(self.name, self.id)

43d116297307639117b7d6e6136b24079c34445d
Thegermanblob <thegermanblob@gmail.com>
2021-08-06 16:10:03 +0000

    Add task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Module shows all states contained in the Db hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+user = sys.argv[1]
+pswd = sys.argv[2]
+dbname = sys.argv[3]
+
+db = MySQLdb.connect("localhost", user, pswd, dbname)
+
+cursor = db.cursor()
+sql = "SELECT * FROM states;"
+try:
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for item in results:
+        print(item)
+except:
+    print("Failed to fetch data")
+
+
+db.close()

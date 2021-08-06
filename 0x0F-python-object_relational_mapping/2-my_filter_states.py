835902f0236d2e2f92db696020cb7fc99079e97f
Thegermanblob <thegermanblob@gmail.com>
2021-08-06 16:19:02 +0000

    Add task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module shows all states contained in the Db hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+user = sys.argv[1]
+pswd = sys.argv[2]
+dbname = sys.argv[3]
+stateName = sys.argv[4]
+
+db = MySQLdb.connect("localhost", user, pswd, dbname)
+
+cursor = db.cursor()
+sql = "SELECT * FROM states WHERE name LIKE '" + stateName + "' ;"
+try:
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for item in results:
+        print(item)
+except:
+    print(sql)
+    print("Failed to fetch data")
+
+
+db.close()

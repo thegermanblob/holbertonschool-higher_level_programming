d3a3044bd67e7f15d96321fc21326bf917e82d62
Thegermanblob <thegermanblob@gmail.com>
2021-08-06 22:26:22 +0000

    Add task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.
+        argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State)
+    instance = states[0]
+    print(str(instance.id)+ ": "+ instance.name)

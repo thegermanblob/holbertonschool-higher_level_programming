8c31043ecbd3747fb54fd94d1613e6bc1391ddcc
Thegermanblob <thegermanblob@gmail.com>
2021-08-07 20:25:14 +0000

    Change constaints of name

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,7 +13,7 @@ class State(Base):
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String(128), nullable=False)
 
     def __repr__(self):
         """ Method for string representation """

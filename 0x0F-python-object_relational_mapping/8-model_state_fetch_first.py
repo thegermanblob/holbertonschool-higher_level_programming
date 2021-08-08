872021cdf276b7fed2155106076d9b43d996f671
Thegermanblob <thegermanblob@gmail.com>
2021-08-08 05:11:07 +0000

    Add table empty check

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,10 +9,13 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.
         argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State)
-    instance = states[0]
-    print(str(instance.id) + ": " + instance.name)
+    states = session.query(State).first()
+    if states is None:
+        print("Nothing")
+    else:
+        instance = states[0]
+        print(str(instance.id) + ": " + instance.name)

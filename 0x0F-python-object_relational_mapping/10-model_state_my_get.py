3eb8264c2e158ff370dd55a6458a7a7fb2567054
Thegermanblob <thegermanblob@gmail.com>
2021-08-08 06:51:16 +0000

    Make pep8 compliant

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""  prints the State object with the name passed as argument from the database """
+"""  prints the State object with the name \
+    passed as argument from the database """
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine, text
@@ -7,8 +8,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.dialects import postgresql
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.
-        argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -17,7 +19,8 @@ if __name__ == "__main__":
         stateNameP = stateName.split(" ", 1)
     except:
         raise ValueError('Incorrect format')
-    query = session.query(State).filter(text("name='{}'".format(stateNameP[0])))
+    query = session.query(State).filter(text("name='{}'"
+                                        .format(stateNameP[0])))
     state = query.first()
     if state is None:
         print("Not found")

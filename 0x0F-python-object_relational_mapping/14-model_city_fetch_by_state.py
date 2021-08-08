7cfda87b4ee3f1317db2cc50169b18e9dcb41f0e
Thegermanblob <thegermanblob@gmail.com>
2021-08-08 20:41:02 +0000

    Add task 14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module lists all State objects from the database """
+import sys
+from model_state import State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State, City).join(City)
+    for _s, _c in states.all():
+        print("{}: ({}) {}".format(_s.name, _c.id, _c.name))

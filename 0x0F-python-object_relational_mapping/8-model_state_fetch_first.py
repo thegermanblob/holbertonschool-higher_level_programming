cde2add323d278b968a8780c040ad66d938091b1
Thegermanblob <thegermanblob@gmail.com>
2021-08-08 05:12:02 +0000

    Fix

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -17,5 +17,5 @@ if __name__ == "__main__":
     if states is None:
         print("Nothing")
     else:
-        instance = states[0]
+        instance = states
         print(str(instance.id) + ": " + instance.name)

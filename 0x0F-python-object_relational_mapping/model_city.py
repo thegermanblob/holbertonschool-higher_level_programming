df1ad7f14b14c045388c9b6ab200409e73f57bca
Thegermanblob <thegermanblob@gmail.com>
2021-08-08 20:56:07 +0000

    Fix model tablename

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-""" Module contains City class """
-from model_state import Base, State
+""" Module contains City lass """
+
+from model_state import  State
 from sqlalchemy import ForeignKey, Column, String, Integer
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
 class City(Base):
     """ Class represents City table pf database """
 
-    ___tablename___ = 'cities'
+    __tablename__ = "cities"
 
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('state.id'))
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)

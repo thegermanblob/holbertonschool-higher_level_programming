ecf98cb1142bde81efa38912b019a8007814e3e0
Thegermanblob <thegermanblob@gmail.com>
2021-08-08 21:04:02 +0000

    Make pep8 compliant

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ Module contains City lass """
 
-from model_state import  State, Base
+from model_state import State, Base
 from sqlalchemy import ForeignKey, Column, String, Integer
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+
 
 class City(Base):
     """ Class represents City table pf database """

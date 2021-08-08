3e5da50c1f0b3509d8ef4fffbd1d884a67bec731
Thegermanblob <thegermanblob@gmail.com>
2021-08-08 05:15:20 +0000

    Make pep8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -7,8 +7,9 @@ from sqlalchemy.orm import sessionmaker
     take 3 arguments: mysql username, mysql password and database name """
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.
-        argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                          (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()

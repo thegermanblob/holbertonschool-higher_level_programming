ef7b3e97294fe69af715c4eb7a3cc1dcef55dc35
Thegermanblob <thegermanblob@gmail.com>
2021-08-08 05:37:48 +0000

    Add doc

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""  prints the State object with the name passed as argument from the database """
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.
+        argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    stateName = sys.argv[4]
+    try:
+        stateNameP = stateName.split(" ", 1)
+    except:
+        raise ValueError('Incorrect format')
+    states = session.query(State).filter(State.name.like(stateNameP))
+    if states is None:
+        print(states)

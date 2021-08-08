e8599b6ad3a91866637c160589302ec2536bf5b0
Thegermanblob <thegermanblob@gmail.com>
2021-08-08 21:01:53 +0000

    idk

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """ Module contains City lass """
 
-from model_state import  State
+from model_state import  State, Base
 from sqlalchemy import ForeignKey, Column, String, Integer
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
 
 class City(Base):
     """ Class represents City table pf database """

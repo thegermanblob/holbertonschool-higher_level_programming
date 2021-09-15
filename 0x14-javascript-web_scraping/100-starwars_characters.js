e798390a306ce29b5f7fa1f836039c340ae18762
Thegermanblob <thegermanblob@gmail.com>
2021-09-15 02:47:02 +0000

    Fix logic && Make semistandard compliant

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   if (!error) {
     const characters = JSON.parse(body).characters;
     characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
+    request(character, function (error, response, body) {
+       if (!error) {
           console.log(JSON.parse(body).name);
         }
       });
-    });
-  }
-});
+  }); 
+}});
+
